6d8d3ec7c2d93a73b53a7d3c3b9ebbaa3b2304ce
Mikkel Ricky <rimi@aarhus.dk>
2017-09-12 14:31:12 +0200

    RADAR-5: Linted JavaScript

--- web/assets/src/survey.js
+++ web/assets/src/survey.js
@@ -1,133 +1,119 @@
+/* global Chart, jQuery */
 // import validation from 'jquery-validation';
 
-(function($) {
-    var loadQuestions = function(survey) {
-				var questions = survey.find('.question').map(function() {
-						var reply = $(this).find('radio');
-						;;; console.log(this);
-						return {
-								title: $(this).find('.title').html(),
-								text: $(this).find('.text').html(),
-								reply:$(this).find('input[type="radio"]:checked').val(),
-								comment: $(this).find('textarea').val(),
-						};
-				}).get();
+(function ($) {
+  var loadQuestions = function (survey) {
+    var questions = survey.find('.question').map(function () {
+      return {
+        title: $(this).find('.title').html(),
+        text: $(this).find('.text').html(),
+        reply: $(this).find('input[type="radio"]:checked').val(),
+        comment: $(this).find('textarea').val()
+      }
+    }).get()
 
-				;;; console.log('questions', questions);
+    return questions
+  }
+  var start = function (selector) {
+    var survey = $(selector)
+    var replies = survey.data('replies')
+    var configuration = survey.data('configuration')
+    var sections = survey.find('section')
+    var questions = loadQuestions(survey)
+    var max = (function () {
+      var max = 0
+      for (var key in configuration.rating) {
+        if (configuration.rating.hasOwnProperty(key)) {
+          var value = parseInt(key)
+          if (value > max) {
+            max = value
+          }
+        }
+      }
 
-				return questions;
-		},
-				start = function(selector) {
-						var survey = $(selector),
-								replies = survey.data('replies'),
-								configuration = survey.data('configuration'),
-								sections = survey.find('section'),
-								questions = loadQuestions(survey),
-								min = $(selector).data('survey-min'),
-								max = (function() {
-										var max = 0;
-										for (var key in configuration.rating) {
-												if (configuration.rating.hasOwnProperty(key)) {
-														var value = parseInt(key);
-														if (value > max) {
-																max = value;
-														}
-												}
-										}
+      return max
+    }())
+    var index = -1
+    var count = sections.length
+    var chart = new Chart(document.getElementById('chart'), {
+      type: 'radar',
+      data: {
+        labels: questions.map(function (question, index) {
+          return (index + 1) + '. ' + question.title
+        }),
+        datasets: [{
+          label: 'Answer',
+          fill: false,
+          backgroundColor: 'red',
+          borderColor: 'blue',
+          pointRadius: 8,
+          data: []
+        }]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scale: {
+          ticks: {
+            min: 0,
+            max: max,
+            stepSize: 1
+          }
+        }
+      }
+    })
 
-										return max;
-								}()),
-								index = -1,
-								count = sections.length,
-								answers = questions.map(function() {
-										return null;
-								}),
-								chart = new Chart(document.getElementById('chart'), {
-										type: 'radar',
-										data: {
-												labels: questions.map(function(question, index) {
-														return (index + 1) + '. ' + question.title;
-												}),
-												datasets: [{
-														label: 'Answer',
-														fill: false,
-														backgroundColor: 'red',
-														borderColor: 'blue',
-														pointRadius: 8,
-														data: [], //questions.map(function() { return null; })
-												}]
-										},
-										options: {
-												// responsive: false,
-												legend: {
-														display: false,
-												},
-												scale: {
-														ticks: {
-																// display: false,
-																// beginAtZero: true,
-																min: 0,
-																max: max,
-																stepSize: 1
-														}
-												}
-										}
-								}),
+    var updateReply = function (index, reply) {
+      chart.data.datasets[0].data[index] = reply
+      chart.update()
+    }
 
-								updateReply = function(index, reply) {
-										chart.data.datasets[0].data[index] = reply;
-										chart.update();
-										;;; console.log('updateReply', index, reply);
-										;;; console.log(chart.data.datasets[0].data[index]);
-								},
+    var showQuestion = function () {
+      if (index < count - 1) {
+        sections.hide().eq(index).show()
+      } else {
+        sections.show()
+      }
+    }
 
-								showQuestion = function() {
-										;;; console.log('showQuestion', index, count);
-										if (index < count - 1) {
-												sections.hide().eq(index).show();
-										} else {
-												sections.show();
-										}
-								},
+    var prev = function () {
+      if (index > 0) {
+        index--
+        showQuestion()
+      }
+    }
 
-								prev = function() {
-										if (index > 0) {
-												index--;
-												showQuestion();
-										}
-								},
-								next = function() {
-										$('form').validate({
-												showErrors: function() {
-														$('#validation-errors').toggle(this.errorList.length > 0);
-												}
-										});
-										if ($('form').valid()) {
-												if (index < count) {
-														// answers[index] = value;
-														index++;
-												}
-												showQuestion();
-										}
-								};
+    var next = function () {
+      $('form').validate({
+        showErrors: function () {
+          $('#validation-errors').toggle(this.errorList.length > 0)
+        }
+      })
+      if ($('form').valid()) {
+        if (index < count) {
+          index++
+        }
+        showQuestion()
+      }
+    }
 
-						if (replies) {
-								replies.forEach(function (reply, index) {
-										updateReply(index, reply.value);
-								});
-						} else {
-								survey.find('.survey-prev').on('click', prev);
-								survey.find('.survey-next').on('click', next);
-								survey.find('input[type="radio"]').on('change', function() {
-										var index = $(this).closest('.question').data('index');
-										;;; console.log('index', index, $(this).val());
-										updateReply(index, $(this).val());
-								});
-								next();
-						}
-				};
+    if (replies) {
+      replies.forEach(function (reply, index) {
+        updateReply(index, reply.value)
+      })
+    } else {
+      survey.find('.survey-prev').on('click', prev)
+      survey.find('.survey-next').on('click', next)
+      survey.find('input[type="radio"]').on('change', function () {
+        var index = $(this).closest('.question').data('index')
+        updateReply(index, $(this).val())
+      })
+      next()
+    }
+  }
 
-		$(function() {
-				start('.survey');
-		});
-}(jQuery));
+  $(function () {
+    start('.survey')
+  })
+}(jQuery))
