0eb4459bf16518fd2389be85462de2fa0a7f1ca9
Mikkel Ricky <rimi@aarhus.dk>
2018-04-04 15:43:04 +0200

    RADAR-38: Updated composer stuff

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity
+ * @UniqueEntity(
+ *   fields="email",
+ *   message="This email is already in use."
+ * )
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
