46b6c123d4fb8a81d8c7cb7c6bb9d4077dfb36b8
Mikkel Ricky <rimi@aarhus.dk>
2021-04-24 08:02:48 +0200

    SUPP0RT-178: Added missing login redirect

--- src/Security/AppLoginFormAuthenticator.php
+++ src/Security/AppLoginFormAuthenticator.php
@@ -100,8 +100,7 @@ class AppLoginFormAuthenticator extends AbstractFormLoginAuthenticator implement
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('easyadmin'));
     }
 
     protected function getLoginUrl()
