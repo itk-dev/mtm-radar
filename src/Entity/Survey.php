291d81983c22eac28dc2c234e1b51d3cd075b9bd
Martin Kristiansen <n1tramkr1st1ansen@gmail.com>
2022-11-01 13:25:01 +0100

    Added Entity&Repository User&Survey

---
+++ src/Entity/Survey.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SurveyRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SurveyRepository::class)]
+class Survey
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $instructions = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $preparations = null;
+
+    #[ORM\Column]
+    private array $configuration = [];
+
+
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getInstructions(): ?string
+    {
+        return $this->instructions;
+    }
+
+    public function setInstructions(?string $instructions): self
+    {
+        $this->instructions = $instructions;
+
+        return $this;
+    }
+
+    public function getPreparations(): ?string
+    {
+        return $this->preparations;
+    }
+
+    public function setPreparations(?string $preparations): self
+    {
+        $this->preparations = $preparations;
+
+        return $this;
+    }
+
+    public function getConfiguration(): array
+    {
+        return $this->configuration;
+    }
+
+    public function setConfiguration(array $configuration): self
+    {
+        $this->configuration = $configuration;
+
+        return $this;
+    }
+
+
+
+
+}
