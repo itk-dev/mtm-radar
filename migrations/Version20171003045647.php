453f81084503916b3586076815bff3781f2bfab5
Martin Kristiansen <n1tramkr1st1ansen@gmail.com>
2022-11-07 13:14:23 +0100

    added migrations from old project

---
+++ migrations/Version20171003045647.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171003045647 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE question DROP description');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE question ADD description LONGTEXT DEFAULT NULL COLLATE utf8_unicode_ci');
+    }
+}
